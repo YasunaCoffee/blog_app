d648f82f08d3eb22c46a8a920471056092b7c64b
amareloscript <yasunaiwase@gmail.com>
2021-12-08 18:21:13 +0900

    add actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,36 @@
 class ArticlesController < ApplicationController
   def index 
       @articles = Article.all
+  end
+
+  def new
+  end
+  
+  def create
+    Article.create(image: article_params[:image], text: article_params[:text])
+  end
+
+  def edit
+    @article = Article.find(id_params[:id])
+  end
+
+  def destroy
+    article = Article.fing(id_params[:id])
+    article.destroy
+  end
+
+  def update
+    article = Atricle.fing(id_params[:id])
+    article.update(article_params)
+  end
+
+  private
+    def article_params
+      params.permit(:image,:text)
     end
+
+    def id_params
+      params.permit(:id)
+    end
+
 end
