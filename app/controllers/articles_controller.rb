aa202686f599c63978c3b0f69c0a5b7b57ded6b6
amareloscript <yasunaiwase@gmail.com>
2021-12-09 12:27:46 +0900

    add render

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only:[:show, :edit, :update]
   
   def index 
-      @articles = Article.all
+    @articles = Article.order(updated_at: :desc)
   end
 
   def new
