ee4bad32e4d6fa4af48ba1fe5b82ac7e71e2ef9a
amareloscript <yasunaiwase@gmail.com>
2021-12-10 12:05:27 +0900

    erb to haml

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,9 @@
 class ArticlesController < ApplicationController
   
   before_action :set_article, only:[:show, :edit, :update, :destroy]
-  
+  before_action :authenticate_user!, only:[:new]
+
+
   def index 
     @articles = Article.order(updated_at: :desc)
   end
