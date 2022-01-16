7328ed76f9a4399b348d69e95dbe7ef793cf34cd
amareloscript <yasunaiwase@gmail.com>
2022-01-16 22:37:29 +0900

    delete comment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -48,7 +48,7 @@ class ArticlesController < ApplicationController
 
   private
     def article_params
-      params.require(:article).permit(:image,:title,:description,:price)
+      params.require(:article).permit(:image,:title,:description)
     end
 
     def set_article
--- db/migrate/20211209081226_add_price_to_article.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToArticle < ActiveRecord::Migration[6.0]
-  def change
-    add_column :articles, :price, :integer
-  end
-end
